89e4c2d94260ba0a03c7e8a9df59f3bde9096f54
Alejandro Kölbl <alejandro.koelbl@gmail.com>
2022-04-11 21:01:01 +0200

    Added clear function and comments

--- WS2812_matrix.py
+++ WS2812_matrix.py
@@ -9,51 +9,68 @@ class WS2812_matrix:
         self.size_x = size_x
         self.size_y = size_y
 
+        # Calculate ranges beforehand so function update is faster
         if self.size_x % 2 == 0:
             self.range_x = range(1, self.size_x, 2)[::-1]
         else:
             self.range_x = range(0, self.size_x, 2)[::-1]
         self.range_y = range(0, self.size_y)
         self.range_y_inv = range(0, self.size_y)[::-1]
-        ORDER = neopixel.RGB
-        self.pixels = neopixel.NeoPixel(board.D18, self.size_x*self.size_y, auto_write=False, pixel_order=ORDER)
+
+        # Initialize NeoPixel class
+        self.pixels = neopixel.NeoPixel(board.D18, self.size_x*self.size_y, auto_write=False, pixel_order=neopixel.RGB)
+
+    # Writes (0, 0, 0) an all LEDs
+    def clear(self):
+        for i in range(self.size_x * self.size_y):
+            self.pixels[i] = (0, 0, 0)
+
+        self.pixels.show()
 
     # 'data' is a 3 dimensional array where the first element represent the y position, the second the x position and
     # the third the RGB value (in that order)
     def update(self, data):
         # WS2812 data enters from the upper right corner and continues to the vertical line
-        i = 0
+        i = 0  # Points to the LEDs position
+
+        # Loops through the columns of the matrix. It starts from the back
         for x in self.range_x:
+            # Writes data (from top to bottom) of the column to the LEDs
             for y in self.range_y:
                 self.pixels[i] = (data[y][x][0], data[y][x][1], data[y][x][2])
                 i += 1
             if x >= 0:
+                # Writes data (bottom up) of the next column to the LEDs
                 x = x - 1
                 for y in self.range_y_inv:
                     self.pixels[i] = (data[y][x][0], data[y][x][1], data[y][x][2])
                     i += 1
 
+        # Writes the values on the LEDs
         self.pixels.show()
 
 
 if __name__ == "__main__":
+    # Dimensions of the display
     size_x = 20
     size_y = 15
 
-    display = WS2812_matrix(size_y, size_x)
-
-    display_data = np.zeros((size_y, size_x, 3), dtype=np.uint8)
+    display = WS2812_matrix(size_y, size_x)  # Declares class
+    display_data = np.zeros((size_y, size_x, 3), dtype=np.uint8)  # Declares class
 
     x = 0
     y = 0
     while True:
+        # Sequence for testing
         if display_data[y][x][0] == 0:
-            display_data[y][x] = (255,255,255) #g,r,b
+            display_data[y][x] = (255, 255, 255)  # R,G,B
         else:
-            display_data[y][x] = (0,0,0)
+            display_data[y][x] = (0, 0, 0)
         print("Update")
-        display.update(display_data)
-        #time.sleep(0.1)
+        display.update(display_data)  # Updates teh data to the LEDs
+        time.sleep(0.1)
+
+        # Sequence for testing
         x += 1
         if x == size_x:
             x = 0
