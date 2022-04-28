b04cb6e456303c8a093ff842f9723feec386758c
Niklas Schneider <SchneiderNiklas97@web.de>
2022-04-28 15:34:38 +0200

    Added all Features

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import numpy as np
 import mediapipe as mp
 import cv2
 import time
+from PIL import Image
 
 WIDTH = 640
 HEIGHT = 480
@@ -17,11 +18,32 @@ Xinit = 0
 widget = 0
 
 
+cap = 0
+CaptureFlag = False
+CameraRenderFlag = False
+TempFlag = False
+
+def cleanUp():
+    global CaptureFlag
+    CaptureFlag = False
+    
+    global CameraRenderFlag
+    CameraRenderFlag = False
+    
+    global TempFlag
+    TempFlag = False
+    global cap
+    if cap != 0:
+        cap.release()
+    cv2.destroyAllWindows()
+    out = np.zeros((15, 20, 3), dtype=np.uint8)
+    display.update(out)
+    
+
 def switchPage(widget):
     if widget == 1:  # Time
         a = False
-        global run_flag
-        print(run_flag, "time")
+        
         pixels = neopixel.NeoPixel(board.D18, 300, brightness=0.2)  # boardD18 = Pin 18, 50=Anzahl LED LED AUS
 
         pixels.fill((0, 0, 0))
@@ -33,7 +55,8 @@ def switchPage(widget):
             blit(out, out_time, (2, 4))
 
             display.update(out)
-        while (run_flag) & (a):  # a mueste eine Timer Var sein
+        global CaptureFlag
+        while (CaptureFlag) & (a):  # a mueste eine Timer Var sein
             out_time = show_time.update()
             if out_time is not None:
                 out = np.full((15, 20, 3), (255, 0, 0), dtype=np.uint8)
@@ -43,13 +66,24 @@ def switchPage(widget):
 
 
     elif widget == -1:  # LED MODE
-        ORDER = neopixel.RGB
-        pixels = neopixel.NeoPixel(board.D18, 300, brightness=0.2,
-                                   pixel_order=ORDER)  # boardD18 = Pin 18, 50=Anzahl LED
-
-        pixels.fill((255, 255, 255))  # pixel blue g,r,b
-
-        pixels.show()  # sudo python3 rgb_test.py
+        temperature = 20.0
+        show_temp = MovingText(str(round(temperature, 1)) + "°C", 20, 150, 500, (255, 0, 255))  # (Text, Pixellaenge, Durchlauf in ms, sleep time after text, Farbe)
+        out = np.zeros((15, 20, 3), dtype=np.uint8)
+     
+        
+        while CaptureFlag:
+             if show_temp.done:
+                 if temperature < 25.0:
+                     temperature += 0.3
+                 else:
+                     temperature = 19
+                 show_temp.update_text(str(round(temperature, 1)) + "°C")
+             out_text = show_temp.update()
+             if out_text is not None:
+                 out = np.zeros((15, 20, 3), dtype=np.uint8)
+                 blit(out, out_text, (1, 0), transparent=True)
+             display.update(out)
+        
 
 
 #### Weiter Widgets hier mit elif
@@ -62,9 +96,9 @@ def SwipeReco(Xcoo):
     return result
 
 
-cap = 0
+
 app = Flask(__name__)
-run_flag = False
+
 
 @app.route("/")
 def home():
@@ -73,11 +107,16 @@ display = WS2812_matrix(15, 20)
 
 @app.route("/Capture")
 def Capture():
-    global run_flag
-    run_flag = True
-
-    while run_flag:
-        global cap
+    cleanUp()
+    global CaptureFlag
+    global CameraRenderFlag
+    global TempFlag
+    TempFlag = False
+    CameraRenderFlag = False
+    CaptureFlag = True
+    
+    while CaptureFlag:
+        
         global WIDTH
         global HEIGHT
         mp_drawing = mp.solutions.drawing_utils
@@ -86,6 +125,7 @@ def Capture():
         widget = 0
 
         cnt = 0
+        global cap
         cap = cv2.VideoCapture(0)
         cap.set(3, 640)
         cap.set(4, 480)
@@ -157,20 +197,33 @@ def Capture():
 
 @app.route("/Shutdown")
 def Shutdown():
-    global run_flag
-    run_flag = False
+    global CaptureFlag
+    CaptureFlag = False
+    
+    global CameraRenderFlag
+    CameraRenderFlag = False
+    
+    global TempFlag
+    TempFlag = False
+    global cap
+    cap.release()
+    cv2.destroyAllWindows()
+    print("Shutdown")
     out = np.zeros((15, 20, 3), dtype=np.uint8)
     display.update(out)
     return render_template("index.html")
 
 @app.route("/LEDTest")
 def ShowTemp():
-    global run_flag
-    run_flag = True
+    cleanUp()
+    global TempFlag
+    TempFlag = True
+    
+    
     temperature = 20.0
     show_temp = MovingText(str(round(temperature, 1)) + "°C", 20, 150, 500, (255, 0, 255))  # (Text, Pixellaenge, Durchlauf in ms, sleep time after text, Farbe)
     out = np.zeros((15, 20, 3), dtype=np.uint8)
-    while run_flag:
+    while TempFlag:
          if show_temp.done:
              if temperature < 25.0:
                  temperature += 0.3
@@ -187,17 +240,28 @@ def ShowTemp():
 
 @app.route("/Gamemode1")
 def RenderCameraCapture():
-     global run_flag
-     run_flag = True
-     cap = cv2.VideoCapture(0)
-     while cap.isOpened():
-         success, image = cap.read()
-         image = cv2.flip(image, 1)
-         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-         image = Image.fromarray(image)
-         image.thumbnail((20, 15))
-         display.update(np.array(image, dtype=np.uint8))
-     cv2.destroyAllWindows()
+     cleanUp()
+     global CaptureFlag
+     CaptureFlag = False
+     global CameraRenderFlag
+    
+     CameraRenderFlag = True
+     global TempFlag
+     TempFlag = False
+
+   
+     
+     while CameraRenderFlag:
+         global cap
+         cap = cv2.VideoCapture(0)
+         while cap.isOpened():
+             success, image = cap.read()
+             image = cv2.flip(image, 1)
+             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+             image = Image.fromarray(image)
+             image.thumbnail((20, 15))
+             display.update(np.array(image, dtype=np.uint8))
+         cv2.destroyAllWindows()
 
      return render_template("index.html")
 
